b1b5996a5fedb992999f9a79c9649a8c6f969ed5
Moghan <anders.kensby@gmail.com>
2016-06-11 14:55:27 +0200

    feat:	Add README file. Refactor code. Push repo to GitHub.

---
+++ README.md
@@ -0,0 +1,40 @@
+# frontend-nanodegree-mobile-portfolio
+
+This is project 5 in my Front-End Developer Nanodegree program.
+
+The challange:
+Create a favorite-places-app using google-map, knockout and a 3rd party api(not google).
+Project details is found [HERE](https://classroom.udacity.com/nanodegrees/nd001/parts/00113454014/modules/271165859175462/lessons/2711658591239847/concepts/26906985370923).
+
+The project repo is available at TODO
+
+Have a look at the neighborhood-map sight at TODO
+
+## Getting started
+Publish on your website,
+
+or run on a local server using Python and ngrok.
+
+```bash
+  $> cd /path/to/your-project-folder
+  $> python -m http.server 8080
+  ```
+
+``` bash
+  $> cd /path/to/your-project-folder
+  $> ngrok http 8080
+  ```
+
+## Build the project
+
+In your project folder:
+
+Install the bower-modules listed as dependencies in the bower.json file into the project folder. At the moment they are the following.
+* knockout
+* bootstrap
+
+Check out the awesomeness of Bower at https://bower.io/
+
+## License
+MIT
+
--- app.js
+++ app.js
@@ -2,210 +2,179 @@ var el_flickrImg = document.getElementById('flickr-img');
 var el_infoContent = document.getElementById('location-info-ID');
 var el_infoContentWiki = document.getElementById('content-wiki-ID');
 
-var CONST = {
-    WIKI_ARTICLES : 1,
-    WIKI_ARTICLE_URLS : 3
-}
 
-var viewModel = {
-  m_title: ko.observable("Adam"),
-  jumboTitle: ko.observable("Bertington"),
+class ViewModel {
+  constructor () {
+    this.locations = ko.observableArray();
+    this.searchString = ko.observable('');
+    this.currLocation = ko.observable(null);
+    this.flickrAndWiki_flag = false;
 
-  locations: ko.observableArray(),
-  searchString: ko.observable(''),
-  currLocation : ko.observable(null)
-}
-
-function searchAltered() {
-  console.log('searchAltered');
-}
-
-//https://en.wikipedia.org/w/api.php?action=query&titles=San_Francisco&prop=images&imlimit=20&format=jsonfm
-
-viewModel.setFlickrImg = function (setId, picId) {
-	//var el_flickrImg = document.getElementById('flickr-img');
-
-	var URL = "https://api.flickr.com/services/rest/" + 
-	"?method=flickr.galleries.getPhotos" +
-	"&api_key=35ea935c9632cde77aec12c324007f6d" +
-	"&gallery_id=" + setId +
-	"&format=json" +
-	"&nojsoncallback=1";
-
-	var _URL = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=35ea935c9632cde77aec12c324007f6d&gallery_id=72157663756796363&format=json&nojsoncallback=1";
-
-	$.getJSON(URL, function(data) {
-		$.each(data.photos.photo, function(i, item) {
-		  var img_src = "http://farm" + item.farm + ".static.flickr.com/" +
-		    item.server + "/" + item.id + "_" + item.secret + "_n.jpg";
-		    if(item.id == picId) {
-		    	el_flickrImg.src = img_src;
-		      //var img_thumb = $("<img/>").attr("src", img_src);
-		      //$(img_thumb).appendTo(".location-info_content_flickr-img");
-		    }
-		});
-	});
-
-}
-
-viewModel.init = function() {
-  //console.log('loadLocations');
-  var url = "locations.json";
-
-  var loadRequestTimeout = setTimeout(function() {
-    console.log('failed to get AJAX resources');
-  }, 4000);
-
-  var Item = function(data) {
-
-    this.lat =  data.lat;
-    this.lng = data.lng;
-    this.title = data.title;
-    this.wikiTitle = data.wikiTitle;
-    this.description = data.description;
-    this.url = data.url;
-    this.isFocused = ko.observable(false);
-    this.setCurrLocation = function() { viewModel.currLocation(this); console.log('toggleFocus');};
-     
-    this.marker = new google.maps.Marker({
-                        map: map,
-                        animation: google.maps.Animation.BOUNCE,
-                        title: this.title,
-                        position: {lat: parseFloat(this.lat), lng: parseFloat(this.lng)}
-                      });
-    this.show = ko.computed(function(){ return this.title.toLowerCase().indexOf(viewModel.searchString().toLowerCase()) >-1; }, this);
-    this.showMarker = ko.computed(function(){ this.show() ? this.marker.setMap(map) : this.marker.setMap(null)}, this);
-    this.animateMarker = ko.computed( function(){ viewModel.currLocation() === this ? this.marker.setAnimation(google.maps.Animation.BOUNCE) : this.marker.setAnimation(null)}, this);
-	this.flickrImg = data.flickrImg;
+    this.addEventListeners();
   }
- 
-  $.ajax({
-    url: url,
-    dataType: "json",
-    success: function(response) {
-      var arr = response.map(function(item) { return new Item(item); });
-      arr.forEach(function(item){ viewModel.locations.push(item)});
 
-      console.log('ajax success');
-      //createMarkerArray();
-      clearTimeout(loadRequestTimeout);
-      //console.log(viewModel.locations()[0].marker);
+// Loads the locations.json file and push each loaction into an array of (location-)items
+// An item also stores data for controlling the google-map-marker for each location.
+  init () {
+    //console.log('loadLocations');
+    var url = "locations.json";
+
+    var loadRequestTimeout = setTimeout(function() {
+      console.log('failed to get AJAX resources');
+    }, 4000);
+
+    var Item = function(data) {
+      this.lat =  data.lat;
+      this.lng = data.lng;
+      this.title = data.title;
+      this.wikiTitle = data.wikiTitle;
+      this.description = data.description;
+      this.url = data.url;
+      this.isFocused = ko.observable(false);
+       
+      this.marker = new google.maps.Marker({
+                          map: map,
+                          animation: google.maps.Animation.BOUNCE,
+                          title: this.title,
+                          position: {lat: parseFloat(this.lat), lng: parseFloat(this.lng)}
+                        });
+      this.show = ko.computed(function(){ return this.title.toLowerCase().indexOf(viewModel.searchString().toLowerCase()) >-1; }, this);
+      this.showMarker = ko.computed(function(){ this.show() ? this.marker.setMap(map) : this.marker.setMap(null)}, this);
+      this.animateMarker = ko.computed( function(){ viewModel.currLocation() === this ? this.marker.setAnimation(google.maps.Animation.BOUNCE) : this.marker.setAnimation(null)}, this);
+      this.flickrImg = data.flickrImg;
     }
-  });
-  
-  this.addEventListeners();
-}
+   
+    $.ajax({
+      url: url,
+      dataType: "json",
+      success: function(response) {
+        var arr = response.map(function(item) { return new Item(item); });
+        arr.forEach(function(item){ viewModel.locations.push(item)});
 
-viewModel.locationButton_onClick = function () {
-  var self = this;
+        clearTimeout(loadRequestTimeout);
+      }
+    });
+    
+    
+  }
 
-  // Current location clicked hides the info-content element
-  if(viewModel.currLocation() === self)
-    el_infoContent.classList.toggle('hide');
-  // Else new current is set and info-content displayed
-  else {
-    viewModel.currLocation(self);
+  loadFlickrImg (setId, picId) {
+    var self = this;
+    // console.log('loadFlickrImg');
+
+    var URL = "https://api.flickr.com/services/rest/" + 
+    "?method=flickr.galleries.getPhotos" +
+    "&api_key=35ea935c9632cde77aec12c324007f6d" +
+    "&gallery_id=" + setId +
+    "&format=json" +
+    "&nojsoncallback=1";
+
+    var _URL = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=35ea935c9632cde77aec12c324007f6d&gallery_id=72157663756796363&format=json&nojsoncallback=1";
+
+    $.getJSON(URL, function(data) {
+      $.each(data.photos.photo, function(i, item) {
+        var img_src = "http://farm" + item.farm + ".static.flickr.com/" +
+          item.server + "/" + item.id + "_" + item.secret + "_n.jpg";
+          if(item.id == picId) {
+            el_flickrImg.src = img_src;
+            self.waitForAllResourcesAndShowLocationInfo()
+          }
+      });
+    });
+
+  }
+
+  showLocationInfo () {
     el_infoContent.classList.remove('hide');
   }
 
-  viewModel.setCurrLocation(self);
-  viewModel.loadData_Wiki(self);
-  viewModel.setFlickrImg(self.flickrImg.album, self.flickrImg.imgId);
-}
-
-viewModel.setCurrLocation = function (clickedLocationBtn) {
-		//var self = this;
-
-		console.log('setCurrLocation');
-		viewModel.currLocation(clickedLocationBtn);
-		
-	}
-
-viewModel.initImages = function(gallery) {
-  var URL = "https://api.flickr.com/services/rest/" + 
-    "?method=flickr.galleries.getPhotos" +
-    "&api_key=35ea935c9632cde77aec12c324007f6d" +
-    "&gallery_id=" + gallery +
-    "&format=json" +
-    "&nojsoncallback=1";
-    
-  var _URL = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=35ea935c9632cde77aec12c324007f6d&gallery_id=72157663756796363&format=json&nojsoncallback=1";
-    
-  $.getJSON(URL, function(data) {
-    $.each(data.photos.photo, function(i, item) {
-      var img_src = "http://farm" + item.farm + ".static.flickr.com/" +
-        item.server + "/" + item.id + "_" + item.secret + "_n.jpg";
-        if(item.id == "25798718074") {
-          var img_thumb = $("<img/>").attr("src", img_src).css("margin", "8px");
-          $(img_thumb).appendTo("#flickr-images");
-        }
-    });
-  });
-  
-}
-
-viewModel.loadData_Wiki = function(clickedLocationBtn) {
-    // console.log(clickedLocationBtn.wikiTitle);
-    var wikiurl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + clickedLocationBtn.wikiTitle;
-    var $wikiElem = $('.location-info_content_wiki');
-
-    var wikiRequestTimeout = setTimeout(function() {
-        $wikiElem.text('failed to get wikipedia resources');
-    }, 2000);
-
-    function addWikiToDom(wikiData) {
-      console.log(wikiData);      
-
-      var pages = wikiData.query.pages;
-      var propertyNames = Object.keys(pages);
-      // console.log(pages[propertyNames[0]].extract);
-      $wikiElem.text(pages[propertyNames[0]].extract);
+  // Shows location-info element after flickr and wiki data is collected
+  // TODO: Must be better ways to do this. +Potential bug(what happens if another button is clicked before all data is collected)
+  waitForAllResourcesAndShowLocationInfo () {
+    if (this.flickrAndWiki_flag) {
+      this.setElementHeights();
+      this.showLocationInfo();
+      this.flickrAndWiki_flag = false;
     }
+    else {
+      this.flickrAndWiki_flag = true;
+    }
+  }
 
-    $.ajax({
-        url: wikiurl,
-        dataType: "jsonp",
-        success: function(response) {
-            //sessionStorage.setItem('wikiData', JSON.stringify(response));
-            addWikiToDom(response);
-            clearTimeout(wikiRequestTimeout);
-        }
-    });
-    //return false;
+  locationButton_onClick  () {
+    var self = this;
+
+    // Current location clicked hides the info-content element
+    if(viewModel.currLocation() === self) {
+      el_infoContent.classList.toggle('hide');
+      viewModel.currLocation(null);
+    }
+    // Else new current is set and info-content displayed
+    else {
+      viewModel.currLocation(self);
+      viewModel.loadData_Wiki(self);
+      viewModel.loadFlickrImg(self.flickrImg.album, self.flickrImg.imgId);
+    }
+  }
+
+  
+  loadData_Wiki (clickedLocationBtn) {
+    var self = this;
+      // console.log('loadData_Wiki');
+      var wikiurl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + clickedLocationBtn.wikiTitle;
+      var $wikiElem = $('.location-info_content_wiki');
+
+      var wikiRequestTimeout = setTimeout(function() {
+          $wikiElem.text('failed to get wikipedia resources');
+          self.waitForAllResourcesAndShowLocationInfo();
+      }, 2000);
+
+      function addWikiToDom(wikiData) {
+        var pages = wikiData.query.pages;
+        var propertyNames = Object.keys(pages);
+        $wikiElem.text(pages[propertyNames[0]].extract);
+      }
+
+      $.ajax({
+          url: wikiurl,
+          dataType: "jsonp",
+          success: function(response) {
+              addWikiToDom(response);
+              clearTimeout(wikiRequestTimeout);
+              self.waitForAllResourcesAndShowLocationInfo();
+          }
+      });
+  }
+
+  // Calculate max-height of the info-content element 
+  setElementHeights () {
+     var imgHeight = el_flickrImg.height;
+     var screenSize = document.documentElement.clientHeight;
+
+     el_infoContentWiki.style.maxHeight = (screenSize - imgHeight) + 'px';
+  }
+
+  onResizeWindow () {
+    viewModel.setElementHeights();
+  }
+
+  addEventListeners () {
+    window.addEventListener('resize', this.onResizeWindow);
+  }
+
+  hideLocationInfo () {
+    document.querySelector(".location-info").classList.add("hide");
+    viewModel.currLocation(null);
+  }
 }
 
-// Calculate max-height of the info-content element, so it does not exceed bottom of browser-window
-viewModel.setElementHeights = function () {
-   var imgHeight = el_flickrImg.height;
-   var screenSize = document.documentElement.clientHeight;
 
-   el_infoContentWiki.style.maxHeight = (screenSize - imgHeight) + 'px';
-}
-
-viewModel.onResizeWindow = function () {
-  viewModel.setElementHeights();
-}
-
-viewModel.addEventListeners = function () {
-  window.addEventListener('resize', this.onResizeWindow);
-  window.addEventListener('ready', this.onResizeWindow);
-}
-
-viewModel.hideLocationInfo = function () {
-  document.querySelector(".location-info").classList.add("hide");
-}
-
-/*viewModel.showLocationInfo = function () {
-  document.querySelector(".location-info").classList.remove("hide");
-}*/
-
+var viewModel = new ViewModel();
 ko.applyBindings(viewModel);
-
 
 var map;
 
 var initMap = function () {
-	console.log('im');
 	map = new google.maps.Map(document.getElementById('map'), {
 	    center: {lat: 59.654159, lng: 12.584117},
 	    zoom: 13
